d41067fd21eef032d115293105dc7cceaeefa631
Eugene-Okhryamkin <eugene.okhryamkin@gmail.com>
2020-12-01 18:20:36 +0300

    task 7 bugfix

--- src/ru/vsu/okhryamkin/Task7/Main.java
+++ src/ru/vsu/okhryamkin/Task7/Main.java
@@ -35,8 +35,10 @@ public class Main {
 
     private static boolean sameElements(int[] arr) {
         for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == arr[i] - 1) {
-                return true;
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[i] == arr[j]) {
+                    return true;
+                }
             }
         }
 
